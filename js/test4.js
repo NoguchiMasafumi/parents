b65067f94ce1064b9ebd1a2c887dc62d2091eb49
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2022-03-29 13:21:24 +0900

    Add files via upload

--- js/test4.js
+++ js/test4.js
@@ -1,12 +1,10 @@
-const VALUE = 1
 export default Object.freeze(
     {
-        VALUE: VALUE,
         quest: [
             {
                 school:'elem'
             },{
-                school:'juni'
+                school:'junior'
             }
         ]
     }
