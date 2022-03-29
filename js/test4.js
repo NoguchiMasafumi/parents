accf543da862f96f785e99e90afab31a564995f5
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2022-03-29 09:51:17 +0900

    Add files via upload

--- js/test4.js
+++ js/test4.js
@@ -1,21 +1,22 @@
 
-export default quest1={
-
-    data(){
-        return{
-            school:'Elementary',
-            year:'1',
-            semester:'',
-            letter:'ss2',
-            figure:'1'
-        },
-        {
-            school:'Elementary',
-            year:'1',
-            semester:'',
-            letter:'dd2',
-            figure:'5'
-        }
+export default {
+    data:{
+        quest:[
+            {
+                school:'Elementary',
+                year:'1',
+                semester:'',
+                letter:'ss2',
+                figure:'1'
+            },
+            {
+                school:'Elementary',
+                year:'1',
+                semester:'',
+                letter:'dd2',
+                figure:'5'
+            }
+        ]
     }
 
 
